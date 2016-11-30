164a709fdc1cd123348be75d085a9c814dd6f2ee
Wie Jie Tjoeng <wie.jie.tjoeng@accenture.com>
2016-11-30 10:48:22 -0600

    Create more microservice projects.

---
+++ config-client-microservice/src/main/java/com/demo/ConfigClientMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientMicroserviceApplication.class, args);
+	}
+}
